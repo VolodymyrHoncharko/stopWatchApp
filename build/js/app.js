719545e1fce2a602ddcc287e314c6a942111f5f9
Volodymyr Honcharko <vhoncharko@gmail.com>
2017-07-06 16:00:56 +0300

    fixed app.js file

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,7 @@
 var app = angular.module('myApp', []);
 
-app.controller('timerCtrl', ['$scope', '$interval', 'timerService', 'lStorage', function ($scope, $interval, timerService, lStorage) {
+
+app.controller('timerCtrl', ['$scope', '$interval', 'timerService', 'lStorage', '$timeout', function ($scope, $interval, timerService, lStorage, $timeout) {
 	'use strict';
 	$scope.timerIsRunning = false;
 	$scope.startTime = 0;
@@ -12,29 +13,63 @@ app.controller('timerCtrl', ['$scope', '$interval', 'timerService', 'lStorage', 
 			ms: '00'
 		};
 	var timeStatus = 100 * (60 * parseInt($scope.timeStatus.minutes)
-										+ parseInt($scope.timeStatus.seconds))
-										+ parseInt($scope.timeStatus.ms);
-
-	// remove the lap
-	$scope.saveToLocalStorage = function () {
-		lStorage.saveData($scope.laps);
-	};
+		+ parseInt($scope.timeStatus.seconds))
+		+ parseInt($scope.timeStatus.ms);
 
 	var lapTimer = function () {
 		$scope.lapTime = timerService.split(timeStatus);
 		$scope.startTime = timeStatus;
 	};
 
+	var startTime = lStorage.getData('startTime');
+
+	if (startTime) {
+		var pauseTime = lStorage.getData('pauseTime');
+		var timeDiff;
+		if (pauseTime) {
+			timeDiff = Math.floor((pauseTime - startTime) / 10);
+		} else {
+			timeDiff = Math.floor(((+new Date) - startTime) / 10);
+		}
+
+		timeStatus = timeDiff;
+
+		$scope.timeStatus = timerService.split(timeDiff);
+
+		if (lStorage.getData('run')) {
+			startTimer();
+		}
+
+	}
+
 	// starts the timer
-	$scope.startTimer = function () {
+	$scope.startTimer = startTimer;
+
+	function startTimer() {
 		$scope.timerIsRunning = true;
 		$scope.startTime = timeStatus;
 
 		var start = document.getElementsByClassName('buttons_link')[0];
 		var pause = document.getElementsByClassName('pause')[0];
+		var status = lStorage.getData('run');
+
+		if (lStorage.getData('lapTime')) {
+			$scope.laps = lStorage.getData('lapTime');
+		}
+
 		start.style.display = 'none';
 		pause.style.display = 'block';
 
+
+		if (!status) {
+			lStorage.saveData('run', true);
+			lStorage.clearData('pauseTime');
+
+		}
+
+		if (!lStorage.getData('startTime')) {
+			lStorage.saveData('startTime', +new Date);
+		}
 
 
 		// clear the interval
@@ -43,16 +78,23 @@ app.controller('timerCtrl', ['$scope', '$interval', 'timerService', 'lStorage', 
 		}
 
 		// updates the timer
-		$scope.timeUpdate = function () {
+		function timeUpdate() {
+			if (!lStorage.getData('run')) {
+				stopTimer();
+			}
 			timeStatus += 1;
 			$scope.timeStatus = timerService.split(timeStatus);
-		};
+		}
+
 		// set the interval
-		$scope.timeRun = $interval($scope.timeUpdate, 10);
-	};
+		$scope.timeRun = $interval(timeUpdate, 10);
+	}
 
 	// stopes the timer
-	$scope.stopTimer = function () {
+	$scope.stopTimer = stopTimer;
+
+
+	function stopTimer() {
 		var start = document.getElementsByClassName('buttons_link')[0];
 		var pause = document.getElementsByClassName('pause')[0];
 		start.style.display = 'block';
@@ -63,8 +105,10 @@ app.controller('timerCtrl', ['$scope', '$interval', 'timerService', 'lStorage', 
 			$interval.cancel($scope.timeRun);
 			lapTimer();
 			$scope.startTime = timeStatus;
+			lStorage.saveData('pauseTime', +new Date);
+			lStorage.saveData('run', false);
 		}
-	};
+	}
 
 	// resets the timer
 	$scope.resetTimer = function () {
@@ -77,7 +121,8 @@ app.controller('timerCtrl', ['$scope', '$interval', 'timerService', 'lStorage', 
 		$scope.timeStatus.seconds = '00';
 		$scope.timeStatus.ms = '00';
 		// clears the local storage
-		lStorage.clearData();
+		lStorage.clearData('startTime');
+		lStorage.clearData('lapTime')
 	};
 
 	// add the lap time
@@ -85,15 +130,33 @@ app.controller('timerCtrl', ['$scope', '$interval', 'timerService', 'lStorage', 
 		if ($scope.timerIsRunning) {
 			lapTimer();
 		}
+
+		var lapTime = lStorage.getData('lapTime');
+		if (lapTime) {
+			$scope.laps = lapTime;
+		}
 		$scope.laps.push($scope.lapTime);
-		$scope.saveToLocalStorage();
+
+		lStorage.saveData('lapTime', $scope.laps);
 	};
 
 	// remove the lap
 	$scope.lapRemove = function (idx) {
 		$scope.laps.splice(idx, 1);
-		$scope.saveToLocalStorage();
+		lStorage.saveData('lapTime', $scope.laps);
 	};
+
+	$interval(function () {
+		var laps = lStorage.getData('lapTime');
+
+		if (!laps) {
+			laps = [];
+		}
+
+		if ($scope.laps.length !== laps.length) {
+			$scope.laps = laps;
+		}
+	}, 100);
 
 }]);
 
@@ -135,17 +198,17 @@ app.factory('lStorage', function ($window, $rootScope) {
 	});
 
 	return {
-		saveData: function (val) {
+		saveData: function (key, val) {
 			var value = JSON.stringify(val);
-			$window.localStorage.setItem('laps_timer', value);
+			$window.localStorage.setItem(key, value);
 			return this;
 		},
-		getData: function () {
-			var value = $window.localStorage.getItem('laps_timer');
+		getData: function (key) {
+			var value = $window.localStorage.getItem(key);
 			return JSON.parse(value);
 		},
-		clearData: function () {
-			return $window.localStorage.removeItem('laps_timer');
+		clearData: function (key) {
+			return $window.localStorage.removeItem(key);
 		}
 	};
 });
